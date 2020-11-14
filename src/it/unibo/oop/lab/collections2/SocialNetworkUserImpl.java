7a0293c2244502d1e2c58bc9cd14764dc0a99830
Valrio Di Zio PC <diziovalerio@gmail.com>
2020-11-14 19:31:12 +0100

    esercizio 2 completato (termine collection, da fare le eccezioni)

--- src/it/unibo/oop/lab/collections2/SocialNetworkUserImpl.java
+++ src/it/unibo/oop/lab/collections2/SocialNetworkUserImpl.java
@@ -1,11 +1,13 @@
 package it.unibo.oop.lab.collections2;
 
-
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
 
 /**
  * 
@@ -17,73 +19,86 @@ import java.util.Map;
  * 1) complete the definition of the methods by following the suggestions
  * included in the comments below.
  * 
- * @param <U>
- *            Specific user type
+ * @param <U> Specific user type
  */
 public class SocialNetworkUserImpl<U extends User> extends UserImpl implements SocialNetworkUser<U> {
-	
-	final Map<String, Collection<U>> mappaFollowed = new HashMap<>();  
 
-    /*
-     * 
-     * [FIELDS]
-     * 
-     * Define any necessary field
-     * 
-     * In order to save the people followed by a user organized in groups, adopt
-     * a generic-type Map:
-     * 
-     * think of what type of keys and values would best suit the requirements
-     */
+	final Map<String, Collection<U>> mappaFollowed;
+	/*
+	 * 
+	 * [FIELDS]
+	 * 
+	 * Define any necessary field
+	 * 
+	 * In order to save the people followed by a user organized in groups, adopt a
+	 * generic-type Map:
+	 * 
+	 * think of what type of keys and values would best suit the requirements
+	 */
 
-    /*
-     * [CONSTRUCTORS]
-     * 
-     * 1) Complete the definition of the constructor below, for building a user
-     * participating in a social network, with 4 parameters, initializing:
-     * 
-     * - firstName - lastName - username - age and every other necessary field
-     * 
-     * 2) Define a further constructor where age is defaulted to -1
-     */
+	/*
+	 * [CONSTRUCTORS]
+	 * 
+	 * 1) Complete the definition of the constructor below, for building a user
+	 * participating in a social network, with 4 parameters, initializing:
+	 * 
+	 * - firstName - lastName - username - age and every other necessary field
+	 * 
+	 * 2) Define a further constructor where age is defaulted to -1
+	 */
 
-    /**
-     * Builds a new {@link SocialNetworkUserImpl}.
-     * 
-     * @param name
-     *            the user firstname
-     * @param surname
-     *            the user lastname
-     * @param userAge
-     *            user's age
-     * @param user
-     *            alias of the user, i.e. the way a user is identified on an
-     *            application
-     */
-    public SocialNetworkUserImpl(final String name, final String surname, final String user, final int userAge) {
-        super(name, surname, user, userAge);
-    }
+	/**
+	 * Builds a new {@link SocialNetworkUserImpl}.
+	 * 
+	 * @param name    the user firstname
+	 * @param surname the user lastname
+	 * @param userAge user's age
+	 * @param user    alias of the user, i.e. the way a user is identified on an
+	 *                application
+	 */
+	public SocialNetworkUserImpl(final String name, final String surname, final String user, final int userAge) {
+		super(name, surname, user, userAge);
+		this.mappaFollowed = new HashMap<>();
+		
+	}
 
-    /*
-     * [METHODS]
-     * 
-     * Implements the methods below
-     */
+	/*
+	 * [METHODS]
+	 * 
+	 * Implements the methods below
+	 */
 
-    @Override
-    public boolean addFollowedUser(final String circle, final U user) {
-    	
-        return false;
-    }
+	@Override
+	public boolean addFollowedUser(final String circle, final U user) {
+		Collection<U> gruppo = this.getFollowedUsersInGroup(circle);
+		if (gruppo!=null && !gruppo.contains(user)) {
+				gruppo.add(user);
+			return true;
+		}
+		return false;
+	}
 
-    @Override
-    public Collection<U> getFollowedUsersInGroup(final String groupName) {
-        return null;
-    }
+	@Override
+	public Collection<U> getFollowedUsersInGroup(final String groupName) {
+		Collection<U> usersToReturn = this.mappaFollowed.get(groupName);
+		if(usersToReturn!= null) {
+			return usersToReturn;
+		}
+		
+		return new ArrayList<>();
+	}
 
-    @Override
-    public List<U> getFollowedUsers() {
-        return null;
-    }
+	@Override
+	public List<U> getFollowedUsers() {
+		Set<String> gruppi = this.mappaFollowed.keySet();
+		Set<U> uniqueUsers = new HashSet<>();
+		List<U> usersToReturn = new ArrayList<>();
+		for(String s : gruppi) {
+			Collection<U> utentiDelGruppoDiRiferimento = this.mappaFollowed.get(s);
+			uniqueUsers.addAll(utentiDelGruppoDiRiferimento);
+		}
+		usersToReturn.addAll(uniqueUsers);
+		return usersToReturn;
+	}
 
 }
